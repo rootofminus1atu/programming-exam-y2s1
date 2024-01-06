0d835802a955946123cc1f617eec634cc9f64d1a
RootOfMinus1ATU <s00240122@atu.ie>
2024-01-06 09:45:21 +0000

    add budgetitem

---
+++ exam/exam/BudgetItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    public enum BudgetItemType
+    {
+        Income,
+        Expense
+    }
+
+    internal class BudgetItem : IComparable<BudgetItem>
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public BudgetItemType ItemType { get; set; }
+        public DateTime Date { get; set; }
+        public bool Recurring {  get; set; }
+
+        public string RecurringStr => Recurring ? "Recurring" : "One Off";
+        
+
+        public BudgetItem(string name, decimal amount, BudgetItemType itemType, DateTime date, bool recurring)
+        {
+            Name = name;
+            Amount = amount;
+            ItemType = itemType;
+            Date = date;
+            Recurring = recurring;
+        }
+
+        public int CompareTo(BudgetItem? other)
+        {
+            return this.Date.Day.CompareTo(other?.Date.Day);
+        }
+
+        public override string ToString()
+        {
+            return $"{Date.Day:00}: {Name} €{Amount} - ({RecurringStr})";
+        }
+
+    }
+}
--- exam/exam/MainWindow.xaml.cs
+++ exam/exam/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,34 @@ namespace exam
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<BudgetItem> budgetItems = new()
+        {
+            new("Grant", 300m, BudgetItemType.Income, new DateTime(2024, 1, 5), true),
+            new("Bonus", 300m, BudgetItemType.Income, new DateTime(2024, 1, 15), false),
+            new("Wages", 100m, BudgetItemType.Income, new DateTime(2024, 1, 25), true),
+
+
+            new("Rent", 400m, BudgetItemType.Expense, new DateTime(2024, 1, 1), true),
+            new("Flight", 100m, BudgetItemType.Expense, new DateTime(2024, 1, 5), false),
+            new("Netflix", 10m, BudgetItemType.Expense, new DateTime(2024, 1, 15), true),
+            new("Spotify", 8m, BudgetItemType.Expense, new DateTime(2024, 1, 20), true),
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BudgetItem b1 = new("Grant", 300m, BudgetItemType.Income, new DateTime(2024, 1, 5), true);
+
+
+
+            foreach (var bi in budgetItems)
+            {
+                Trace.WriteLine(bi);
+            }
         }
     }
 }
